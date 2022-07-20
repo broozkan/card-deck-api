0742520e3682cf5890132aaf7217c39148427517
broozkan <burhan.ozkan@live.com>
2022-07-21 00:40:35 +0300

    Created starter server and configured eslint prettier and jest

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { deckController } from './controllers';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
