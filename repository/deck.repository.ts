3d9e0234f49c58919b2fc68e741282aaff568eba
broozkan <burhan.ozkan@live.com>
2022-07-24 17:04:00 +0300

    Repository layer for deck domain

--- repository/deck.repository.ts
+++ repository/deck.repository.ts
@@ -1,3 +1,4 @@
+import httpStatus from "http-status"
 import { Deck } from "../models"
 import { IDeck } from "../models/deck.model"
 import { ResponseType } from "../types/response.type"
@@ -9,12 +10,46 @@ const saveDeck = async (deck: IDeck): Promise<ResponseType> => {
         res.data = result
         res.isSuccess = true
     } catch (err: any) {
-        res.error = err._message
+        res.error = err
+        res.isSuccess = false
+    }
+    return res
+}
+
+const getDeck = async (deckId: String): Promise<ResponseType> => {
+    var res: ResponseType = { isSuccess: false }
+    try {
+        let result = await Deck.findOne({ deckId }, { "cards._id": 0 })
+        if (!result) {
+            res.error = "document not found"
+            res.statusCode = httpStatus.NOT_FOUND
+            res.isSuccess = false
+        } else {
+            res.data = result
+            res.isSuccess = true
+        }
+    } catch (err: any) {
+        res.error = err
+        res.isSuccess = false
+    }
+    return res
+}
+
+const updateDeck = async (id: String, deck: IDeck): Promise<ResponseType> => {
+    var res: ResponseType = { isSuccess: false }
+    try {
+        let result = await Deck.findByIdAndUpdate(id, deck)
+        res.data = result
+        res.isSuccess = true
+    } catch (err: any) {
+        res.error = err
         res.isSuccess = false
     }
     return res
 }
 
 export {
-    saveDeck
+    saveDeck,
+    getDeck,
+    updateDeck
 }
