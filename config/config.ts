773f3eb7601143e7e236d03d20f5caa0bf5f6eea
broozkan <burhan.ozkan@live.com>
2022-07-24 16:59:58 +0300

    Add extra config fields

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,18 @@
-export const config = {
-    port: process.env.PORT
+import { load } from 'ts-dotenv';
+
+const env = load({
+    MONGODB_CONNECTION_URL: String,
+    MONGODB_CONNECTION_TEST_URL: String,
+    PORT: Number
+})
+
+export default {
+    port: env.PORT,
+    mongoose: {
+        url: env.MONGODB_CONNECTION_URL as string,
+        testUrl: env.MONGODB_CONNECTION_TEST_URL as string,
+        options: {
+
+        }
+    }
 }
